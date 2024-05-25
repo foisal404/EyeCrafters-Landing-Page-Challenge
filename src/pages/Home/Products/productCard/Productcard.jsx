0d8b5141e2f72fb4dcc2811fecff419019c95e3b
foisal404 <foisalahmmed2@gmail.com>
2024-05-25 22:40:01 +0600

    Implement CLick to add product in local storage

--- src/pages/Home/Products/productCard/Productcard.jsx
+++ src/pages/Home/Products/productCard/Productcard.jsx
@@ -4,6 +4,19 @@ import Saleicon from "../../../../assets/icons/Sale.png";
 import Slidericon from "../../../../assets/icons/Slider.png";
 
 const Productcard = ({ image, discount, data }) => {
+  // Event handler to store data in localStorage
+  const handleAddToCart = () => {
+    const productData = {
+      name: data?.name ? data?.name : "NAME",
+      Offer: data?.offer ? data?.offer : "0",
+      Price: data?.price ? data?.price : "0",
+      image: image ? image : "IMAGE",
+      discount: discount ? "YES" : "NO",
+    };
+    const existingProducts = JSON.parse(localStorage.getItem("products")) || [];
+    existingProducts.push(productData);
+    localStorage.setItem("products", JSON.stringify(existingProducts));
+  };
   return (
     <div className="max-w-[417px] max-h-[630px] ">
       <div className="bg-[#F7F7F7] p-9">
@@ -34,7 +47,10 @@ const Productcard = ({ image, discount, data }) => {
               )}
             </p>
           )}
-          <button className="pt-5 font-normal text-xl dark-Color border-b-2  border-b-[#5A5A5A]">
+          <button
+            className="pt-5 font-normal text-xl dark-Color border-b-2  border-b-[#5A5A5A]"
+            onClick={handleAddToCart}
+          >
             Add To Cart
           </button>
         </div>
